f8f2e7515e81cf64040a8aff64edb549d3e64f58
Afranio Nunes Dantas <afraniodantas0224@gmail.com>
2024-10-14 22:02:01 -0300

    feat: add integration on ms-notification

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './modules/users/user.module';
 import { LoginModule } from './modules/login/login.module';
 import { TaskUserModule } from './modules/tasks/taskUser.module';
+import { NotificationModule } from './modules/notification/notification.module';
 
 @Module({
-  imports: [UserModule, LoginModule, TaskUserModule],
+  imports: [UserModule, LoginModule, TaskUserModule, NotificationModule],
   controllers: [],
   providers: [],
 })
